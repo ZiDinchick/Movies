cb9bd41028c5e7c41a227ecb36ffaae842da31e1
ZiDinchick <fredy.04@mail.ru>
2024-07-09 22:49:49 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Movies'
   plugins: [react()],
-  base: 'Movies/deployments/github-pages/'
 })
